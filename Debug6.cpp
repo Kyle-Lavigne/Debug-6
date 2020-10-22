ce7f91549e548ddf7882af11ee6c416021251a47
Kyle-Lavigne <72029374+Kyle-Lavigne@users.noreply.github.com>
2020-10-22 09:15:52 -0500

    Update Debug6.cpp

--- Debug6.cpp
+++ Debug6.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <array>
 #include <string>
-#include <algorithm>
 using namespace std;
 
 /*
@@ -18,16 +17,22 @@ meant to check if the code is actually in order.
 int arr[] = {20,12,65,45,98,65,85,12,54,78,55,13,50,19,84,20,43,10};
 int arrSize =  sizeof(arr)/sizeof(arr[0]);
 int main() {
-int j = arrSize;
+
   //Bit of code for sorting
-sort(arr, arr+j);
-reverse(arr +0, arr + j);
+  for (unsigned j=arrSize;j>0;j--){
+    for (unsigned i=1;i<j;i++){
+      if (arr[i-1]>arr[i]){
+        arr[i-1] = arr[i];
+        arr[i] = arr[i-1];
+        }
+    }
+  }
 
   // Bit of code to check if sort worked
   // HINT : This bit of code is just fine
   bool checkFlag = true;
   for (unsigned i=1;i<arrSize;i++){
-    if (arr[i]>arr[i-1]){checkFlag = false;}
+    if (arr[i]<arr[i-1]){checkFlag = false;}
   }
   if (checkFlag) {cout << "Sorted properly";}
   else {cout << "Not sorted properly";}
